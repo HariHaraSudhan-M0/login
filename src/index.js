4e1395ff31891fb0a6b3ccd1b226486d761556a3
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-06-03 19:23:27 +0530

    message for running the server

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ const path = require('path')
 const bcrypt = require('bcrypt')
 
 const app = express();
+
+
+
+
+
+const port = 5000;
+app.listen(port,()=>{
+    console.log(`server running in the port: ${port}`);
+})
